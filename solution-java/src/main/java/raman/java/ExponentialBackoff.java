a127240ffa1de1aec067f04804cf338f8e5c86a8
raman <raman@tetrisinnovations.com>
2025-12-24 11:41:20 +0530

    Exponential-backoff-jitter-solution

---
+++ solution-java/src/main/java/raman/java/ExponentialBackoff.java
@@ -0,0 +1,58 @@
+package raman.java;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ExponentialBackoff {
+
+    public static void hittingUrlWithJitter (String url, int maxSteps, long timeoutSeconds) {
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .timeout(Duration.ofSeconds(timeoutSeconds))
+                .build();
+
+        for (int i = 1; i <= maxSteps; i++){
+            long startTime = System.currentTimeMillis();
+
+            try{
+                System.out.println("Attempt :" + i );
+
+                HttpResponse<Void> response = httpClient.send(httpRequest,HttpResponse.BodyHandlers.discarding());
+
+                if (response.statusCode() == 200){
+                    long timeTaken = System.currentTimeMillis() - startTime;
+                    System.out.println("SUCCESS! "+ timeTaken + " ms taken");
+                    return;
+                } else {
+                    System.out.println("Failed :" + response.statusCode());
+                }
+
+            }catch (Exception e){
+                System.out.println("Failed to connect " + e.getMessage());
+            }
+
+            if (i == maxSteps){
+                System.out.println("STOPPED. Max attempts reached");
+                break;
+            }
+
+            long jitterSleep = ThreadLocalRandom.current().nextLong(1000, 3000);
+            System.out.println("Waiting "+ jitterSleep + "ms before next retry");
+
+            try {
+                Thread.sleep(jitterSleep);
+            }catch (InterruptedException e){}
+
+        }
+    }
+
+    public static void main(String[] args){
+//        hittingUrlWithJitter("https://www.google.com",5);
+        hittingUrlWithJitter("https://www.raman.com",3,3);
+    }
+}
